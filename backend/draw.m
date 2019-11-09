ad4a04d3bc74306e7619518ab29b1700d2205be6
Yurematias <yurematias@ufrn.edu.br>
2019-11-09 13:54:43 -0300

    octave algorithm updated

--- backend/draw.m
+++ backend/draw.m
@@ -1,10 +1,12 @@
 x = load("D:/Documentos/ProjetosWeb/SmartSilo/backend/hist.txt");
-h = 0.25;
-nAmostras = length(x(:,1));
-tMax = nAmostras*h;
-t = 0:h:tMax-h; % for t = 0; t <= tMax; t+=h                                         
+freq = 0.25;
+nSamples = length(x(:,1));
+maxTime = nSamples*freq;
+% equivale a for t = 0; t <= tMax; t+=h        
+% no octave arrays começam do 1    
+t = 0: freq : maxTime-freq;                               
 
-fh = figure();
+fh = figure();  
 
 plot(t, x(:,1), t, x(:,4));
 grid;
@@ -12,10 +14,6 @@ title('Controle de temperatura interna do silo');
 xlabel('Tempo');
 ylabel('oC');
 
-%print(fh, "img.pdf", "-dpdflatexstandalone");
-%system("pdflatex img");
 print -dpng "D:/Documentos/ProjetosWeb/SmartSilo/frontend/img/chart.png";
 
 close(fh); % destruir janela
-
-%savefig('img.fig');
