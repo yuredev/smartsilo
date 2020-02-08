6dc99069b029d35690617b9c4495f44e4dd383a5
Yurematias <yurematias@ufrn.edu.br>
2020-02-08 19:25:53 -0300

    exemplo de side menu adicionado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,3 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Frontend VueJS</h1>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
