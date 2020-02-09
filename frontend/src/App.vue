46bcafab2d0c99c8cfa1c49905470589f3e3752c
Yurematias <yurematias@ufrn.edu.br>
2020-02-08 22:53:13 -0300

    sidenav separada em outro componente

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="background">
-    <div id="mySidenav" class="sidenav" :style="styles.sideNavData">
+  <div>
+    <!-- <div class="sidenav" :style="styles.sideNavData">
       <span id="menuHamburger" class="hamburger" @click="closeNav">&#9776; close</span>
       <a href="#">About</a>
       <a href="#">Services</a>
       <a href="#">Clients</a>
       <a href="#">Contact</a>
-    </div>
+    </div> -->
+    <SideNav @closeNav="closeNav()" :data="styles.sideNavData"/>
 
     <div id="main" :style="styles.mainData">
       <span id="outerHamburger" class="hamburger" :style="styles.outerHamburgerData"  @click="openNav">&#9776; open</span>
@@ -16,7 +17,14 @@
 </template>
 
 <script>
+
+import SideNav from './components/SideNav'
+
+
 export default {
+  components: {
+    SideNav
+  },
   data() {
     return {
       styles: {
@@ -33,7 +41,7 @@ export default {
     }
   },
   mounted() {
-    this.openNav();
+    // this.openNav();
   },
   methods: {
     closeNav() {
@@ -51,50 +59,13 @@ export default {
 </script>
 
 <style scoped>
-  
-  .background {
-    background-color: black;
-    height: 100%;
-  }
-
-  .sidenav {
-    height: 100%;
-    width: 0;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background-color: #114;
-    overflow-x: hidden;
-    transition: 0.5s;
-    padding-top: 60px;
-  }
-
-  .sidenav a {
-    padding: 8px 8px 8px 32px;
-    text-decoration: none;
-    font-size: 25px;
-    color: #818181;
-    display: block;
-    transition: 0.3s;
-  }
-
-  .sidenav a:hover {
-    color: #f1f1f1;
-  }
-
+   
   .hamburger{
     font-size:30px;
+    cursor:pointer; 
       cursor:pointer; 
-      color: white;
-  }
-
-  #outerHamburger{
-    background-color: "white";	
-  }
-
-  #menuHamburger{
-    background-color: "white";
+    cursor:pointer; 
+    color: white;
   }
 
   #main {
@@ -102,8 +73,4 @@ export default {
     padding: 16px;
   }
 
-  @media screen and (max-height: 450px) {
-    .sidenav {padding-top: 15px;}
-    .sidenav a {font-size: 18px;}
-  }
 </style>
---
+++ frontend/src/components/SideNav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="sidenav" :style="data">
+    <span id="menuHamburger" class="hamburger" @click="closeNav">&#9776; close</span>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {width: '0px'}
+      }
+    }
+  },
+  methods: {
+    closeNav() {
+      this.data.width = '0px';
+      this.$emit('closeNav');
+    }
+  },
+}
+</script>
+
+
+<style scoped>
+  .sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    transition: 0.3s;
+  }
+
+  .sidenav a:hover {
+    color: #f1f1f1;
+  }
+
+  @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+  }
+
+  .sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #114;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+  }
+</style>
