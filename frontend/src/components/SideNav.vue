487afd9446fb24054a9b3b3a2d6a1cc23705a2c5
Yurematias <yurematias@ufrn.edu.br>
2020-02-11 00:00:19 -0300

    estilos css mudados

--- frontend/src/components/SideNav.vue
+++ frontend/src/components/SideNav.vue
@@ -6,6 +6,26 @@
         <img src="../assets/smart.png" alt="logo do sistema" id="logo">
       </div>
     </div>
+    <div class="formsArea">
+      <div class="centralize-self" style="margin-top: 10px;">
+        <label for="setpoint">Setpoint: </label>
+        <input type="number" name="setpoint" id="">
+      </div>
+      <div class="centralize-self">
+        <label for="controlMode">Modo de controle:</label>
+        <select name="controlMode" id="">
+          <option value="">Malha aberta</option>
+          <option value="">PID</option>
+          <option value="">ON/OFF</option>
+        </select>
+      </div>
+      <div class="radioButtonDiv">
+        <label for="voltage0v">0v</label> 
+        <input type="radio" name="voltageRadioBtn" id="voltage0v">
+        <label for="voltage3v">3v</label> 
+        <input type="radio" name="voltageRadioBtn" id="voltage3v">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,13 +53,18 @@ export default {
 </script>
 
 <style scoped>
+  .centralize-self{
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+  }
   .sidenav *{
     margin: 0px 0px 0px 0px;
     border: 0;
     padding: 0;
   }
   .sidenav > * {
-    margin: 0px 0px 0px 15px;
+    margin: 0px 15px 4px 15px;
   }
 
   .sidenav {
@@ -65,14 +90,40 @@ export default {
 
   #logo{
     width: 180px;
-    margin-top: 3px;
-    margin-left: 14px;
+    margin-top: 5px;
+    margin-bottom: 40px;
+    margin-left: 17px;
   }
   
   @media screen and (max-width: 992px){
     #logo{
       display: none;
     }
+  }
+
+  input, select{
+    width: 190px;
+  }
+
+  .formsArea{
+    background-color: #00000021;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    /* align-items: center; */
+  }
+  .formsArea * {
+    margin-bottom: 5px;
+  }
+  .formsArea > * {
+    margin-left: 4px;
+  }
+  .radioButtonDiv{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100px;
+    align-self: flex-end;
   }
 
 </style>
