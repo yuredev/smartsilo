4c4ed3c8be8c98a86efea417952623b083d64016
Yurematias <yurematias@ufrn.edu.br>
2020-02-26 00:41:05 -0300

    console.info removed

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,16 @@
+// module.exports = {
+//   presets: [
+//     '@vue/cli-plugin-babel/preset'
+//   ]
+// }
+
 module.exports = {
-  presets: [
+  "presets": [
     '@vue/cli-plugin-babel/preset'
-  ]
-}
+  ],
+  "env":{
+     "production": {
+         "plugins": ["transform-remove-console"]
+     }
+  } 
+} 
