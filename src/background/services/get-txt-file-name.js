e5fc939e878bc21c1c59d19abf0acd7882e65423
yurematias <yurematias@ufrn.edu.br>
2020-07-11 01:50:05 -0300

    files removed

--- src/background/services/get-txt-file-name.js
+++
@@ -1,4 +0,0 @@
-module.exports = () => {
-  const time = new Date();
-  return `${time.getDate()}-${time.getMonth() + 1}-${time.getUTCFullYear()}-${time.getHours()}-${time.getMinutes()}-${time.getSeconds()}`;
-}
