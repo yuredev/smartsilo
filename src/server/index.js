fcca7e9e888ccfc3841dd5470affe0c69517148f
Yure Matias <yurematias@ufrn.edu.br>
2020-08-21 13:13:54 -0300

    server updated

--- src/server/index.js
+++ src/server/index.js
@@ -5,8 +5,7 @@ const app = express();
 const WEB_SOCKET_PORT = 3000;
 const HTTP_PORT = 3333;
 
-
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   console.log(`New connection with the id: ${socket.id}`);
 });
 
@@ -16,4 +15,3 @@ app.listen(HTTP_PORT, () => {
 
 io.listen(WEB_SOCKET_PORT);
 console.log(`Websocket Working at: http://localhost:${WEB_SOCKET_PORT}`);
-
